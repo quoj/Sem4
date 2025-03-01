62fd2f65bef7af1a3579bb4504f62ec57380317c
Trần Lê Tuấn Anh <133200203+quoj@users.noreply.github.com>
2025-03-01 11:25:05 +0700

    Add files via upload

---
+++ android/app/src/main/java/com/example/t2305m_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.t2305m_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:t2305m_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp() as Widget);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
